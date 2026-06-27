0b808b885d7133f3e7f9ab72897fcdfc3b534fa4
u-harshitha007 <harshithauppar203@gmail.com>
2026-06-28 00:00:47 +0530

    Update welcome.jsx

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -3,6 +3,8 @@ function Welcome() {
     <>
       <h1>PyStack Learn</h1>
       <p>Welcome Harshitha!</p>
+      <h2>day 2 of learning react</h2>
+      <p>todays topic is jsx</p>
       <button>Start Learning</button>
     </>
   );
